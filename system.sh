9441032e0c14b914c1e43d26171e00d8048e9824
yanyan <141106887+yyzz521@users.noreply.github.com>
2025-10-01 19:49:22 +0800

    Add files via upload

---
+++ system.sh
@@ -0,0 +1,3 @@
+sudo apt install -y gnome-shell gnome-shell-extension-manager gnome-tweaks flatpak
+sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+sudo apt install -y ./Clash.Verge_2.4.2_arm64.deb
